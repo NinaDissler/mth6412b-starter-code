20a7664e3df6d5c262b93f547941bc5869cc43a8
Trang Vu <trang.vu@blindspot.ai>
2018-10-10 21:32:06 -0400

    Clean the code.

--- projet/phase2/connected-component.jl
+++ projet/phase2/connected-component.jl
@@ -5,27 +5,20 @@ abstract type AbstractConnectedComponent{T} end
 
 """Type representant une composante connexe"""
 mutable struct ConnectedComponent{T} <: AbstractConnectedComponent{T}
-		#name::String
-        #graph::Graph{T}
         nodes::Vector{Node{T}}
         edges::Vector{Edge{T}}
 end
 
+"""Constructeur"""
 ConnectedComponent() = ConnectedComponent(Node{Any}[],Edge{Any}[])
 
-#"""Renvoie le nom de la composante connexe."""
-#name(component::AbstractConnectedComponent) = component.name
-
-#""" Renvoie le graphe contenant la composante connexe"""
-#graph(component::AbstractConnectedComponent) = component.graph
-
-""" Renvoie les noeuds de la composante connexe"""
+"""Renvoie les noeuds de la composante connexe"""
 nodes(component::AbstractConnectedComponent) = component.nodes
 
 """Renvoie le nombre de noeuds de la composante connexe"""
 nb_nodes(component::AbstractConnectedComponent) = length(component.nodes)
 
-""" Renvoie les arêtes de la composante connexe"""
+"""Renvoie les arêtes de la composante connexe"""
 edges(component::AbstractConnectedComponent) = component.edges
 
 """Renvoie le nombre d'arêtes de la composante connexe"""
@@ -45,11 +38,8 @@ end
 
 """Affiche une composante connexe"""
 function show(component::ConnectedComponent)
-	#component_name = name(component)
-	#component_graph = graph(component)
 	component_nb_nodes = nb_nodes(component)
 	component_nb_edges = nb_edges(component)
-	#println("Connected component ", component_name, " in ", name(component_graph)," has ", component_nb_nodes, " nodes")
 	println("Connected component has ", component_nb_nodes, " nodes")
 	for node in nodes(component)
 		show(node)
