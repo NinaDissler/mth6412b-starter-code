92a7eac96290dca4ccfb44d4ad26d110ab7ec68a
NinaDissler <39528116+NinaDissler@users.noreply.github.com>
2018-10-30 14:32:24 -0400

    Update distance.jl

--- projet/distance.jl
+++ projet/distance.jl
@@ -1,10 +1,10 @@
-function distance(nodeA::AbstractNode,nodeB::AbstractNode,graph)
+function distance(nodeA::AbstractNode,nodeB::AbstractNode,graph::AbstractGraph)
+  distance=Inf
   for edge in edges(graph)
-    distance=Inf
     if nodes(edge)[1]==nodeA && nodes(edges)[2]==nodeB
       distance=weigth(edge)
     elseif nodes(edge)[2]==nodeA && nodes(edges)[1]==nodeB
       distance=weigth(edge)
-     end
-     end
-     end
+    end
+  end
+end
