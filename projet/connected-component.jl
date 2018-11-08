b35e07a3c8f92c3ea856139bda79b15984a9fb59
NinaDissler <39528116+NinaDissler@users.noreply.github.com>
2018-11-08 11:27:41 -0500

    CC inherits of AbstractGraph

--- projet/connected-component.jl
+++ projet/connected-component.jl
@@ -1,36 +1,9 @@
 import Base.show
 
-""" Type abstrait dont d'autres types de composantes connexes deriveront"""
-abstract type AbstractConnectedComponent{T} end
-
-"""Type representant une composante connexe"""
-mutable struct ConnectedComponent{T} <: AbstractConnectedComponent{T}
+"""Type representant une composante connexe : herite des méthodes de AbstractGraph"""
+mutable struct ConnectedComponent{T} <: AbstractGraph{T}
         nodes::Vector{Node{T}}
         edges::Vector{Edge{T}}
-end
-
-"""Renvoie les noeuds de la composante connexe"""
-nodes(component::AbstractConnectedComponent) = component.nodes
-
-"""Renvoie le nombre de noeuds de la composante connexe"""
-nb_nodes(component::AbstractConnectedComponent) = length(component.nodes)
-
-"""Renvoie les arêtes de la composante connexe"""
-edges(component::AbstractConnectedComponent) = component.edges
-
-"""Renvoie le nombre d'arêtes de la composante connexe"""
-nb_edges(component::AbstractConnectedComponent) = length(component.edges)
-
-"""Ajoute un noeud à la CC."""
-function add_node!(cc::ConnectedComponent{T}, node::Node{T}) where T
-	push!(cc.nodes, node)
-	cc
-end
-
-"""Ajoute une arrête à la CC."""
-function add_edge!(cc::ConnectedComponent{T}, edge::Edge{T}) where T
-	push!(cc.edges, edge)
-	cc
 end
 
 """Affiche une composante connexe"""
