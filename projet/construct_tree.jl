d6fb128b906661bb3ed62f6f839d95078db47469
NinaDissler <39528116+NinaDissler@users.noreply.github.com>
2018-11-08 14:21:15 -0500

    Update construct_tree.jl

--- projet/construct_tree.jl
+++ projet/construct_tree.jl
@@ -2,6 +2,14 @@ include("tree.jl")
 include("graph.jl")
 include("connected-component.jl")
 
-function construct_tree(name::String, graph::AbstractGraph{T}, root::AbstractNode{T}) where R
-
+function construct_tree(name::String, graph::AbstractGraph{T}, root::AbstractNode{T}) where T
+  tree=Tree(name,root,nothing,nothing)
+  copiedgraph=copy(graph)
+  length(neighbours(root))==0 && return tree
+  for son in neighbours(root,copiedgraph)
+    delete_node!(root,copiedgraph)
+    tson=construct_tree(name,graph,son)
+    add_son!(tree,tson)
+    set_parent!(tson,tree)
+  end
 end
