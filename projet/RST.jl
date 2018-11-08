c212b4339029004beb0d66ea097ef7293c328253
NinaDissler <39528116+NinaDissler@users.noreply.github.com>
2018-11-08 11:31:05 -0500

    Update RST.jl

--- projet/RST.jl
+++ projet/RST.jl
@@ -7,7 +7,9 @@ include("preordre.jl")
 """ Algorithme de Rosenkrantz, Stearns et Lewis de calcul d'une tournée minimale approchée"""
 function RST(graph::AbstractGraph{T},root::AbstractNode{T},choice::Union{"prim","kruskal"}) where T
   " Calcul d'un arbre de recouvrement minimal avec l'algo de son choix "
+  " Prim : renvoie un graphe de recouvrement de type Graph"
   choice=="prim" && MST=prim(graph,root)
+  " Kruskal : renvoie une composante connexe de type ConnectedComponent"
   choice=="kruskal" && MST=kruskal(graph)
   
   @test (nb_nodes(MST)!=0)
