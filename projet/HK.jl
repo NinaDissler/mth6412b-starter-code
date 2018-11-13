82cef1332669708e57bf0896f217dace688471e5
NinaDissler <39528116+NinaDissler@users.noreply.github.com>
2018-11-13 11:21:13 -0500

    Update HK.jl

--- projet/HK.jl
+++ projet/HK.jl
@@ -4,4 +4,11 @@ include("prim.jl")
 include("tree.jl")
 
 function HK(graph::AbstractGraph, root::AbstractNode, choix::Union{"prim","kruskal"}
+    K=0
+    N=nb_nodes(graph)
+    pi_0=Vector{Int64}[]
+    for i in (1,N)
+      push!(pi_0,0)
+    end
+    Wopt=-Inf
 end
