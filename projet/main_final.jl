29efb13a2f500b685e2cc6c1395842c39efe4943
NinaDissler <39528116+NinaDissler@users.noreply.github.com>
2018-12-12 22:05:29 -0500

    Update main_final.jl

--- projet/main_final.jl
+++ projet/main_final.jl
@@ -1,18 +1,28 @@
-import Base.show
+# import Base.show
 using FileIO
-using Images
-using ImageView
+# using Images
+# using ImageView
 
-include("stsp2graph.jl")
+include("node.jl")
+include("edge.jl")
+include("graph.jl")
+include("tsp2graph.jl")
+include("read_stsp.jl")
 include("kruskal.jl")
 include("RSL.jl")
 include("eval_weights.jl")
-include("tsp2tour.jl")
-include("bin/tools.jl")
+include("graph2tour.jl")
+include("../bin/tools.jl")
+include("neighbours.jl")
 
-image_file="tsp/instances/abstract-light-painting.tsp"
-filename_tour="tours/tour_solution1.tour"
-tsp2tour(image_file,filename_tour)
-shuffled_image="images/shuffled/abstract-light-painting.png"
-reconstructed_image="images/reconstructed/abstract-light-painting-mysolution.png"
-reconstruct_picture(filename_tour,shuffled_image,reconstructed_image)
+image_file="../tsp/instances/abstract-light-painting.tsp"
+filename_tour="../tsp/tours/tour_solutiontest.tour"
+shuffled_file="../images/shuffled/abstract-light-painting.png"
+reconstructed_image="../images/reconstructed/mysolution1.png"
+println("//Construction du graphe")
+g=tsp2graph(image_file)
+println("********************************")
+println("//Construction et écriture du tour")
+graph2tour(g,filename_tour)
+println("********************************")
+# reconstruct_picture(filename_tour,shuffled_file,reconstructed_image)
